506ad27c9d2c92cac32a1990c830928c54fa61ca
luojia65 <me@luojia.cc>
2018-08-12 19:18:03 +0800

    .

--- src/block.rs
+++ src/block.rs
@@ -1,7 +1,7 @@
 use crate::pos::BlockPos;
-use crate::id::Id;
+use crate::id::BlockId;
 
-// id and meta, including facing
+// basic data of blocks, including id, meta and sometimes facing
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct BlockState {
     runtime_internal: u16
@@ -11,7 +11,7 @@ use std::io::Error as IoError;
 
 #[derive(Debug)]
 pub enum Error {
-    BlockNotFound,
+    UnsupportedBlock,
     IoError(IoError)
 }
 
@@ -22,112 +22,66 @@ impl From<IoError> for Error {
 }
 
 use std::result::Result as StdResult;
+
+// result of block operations
 pub type Result<T> = StdResult<T, Error>;
 
+// buffer of a block.
+// some blocks need a buffer to store extra value other than its state,
+// such as text lines for signs, orientation for skulls, content for signs, etc
 pub type BlockBuf = Vec<u8>;
 
 // runtime system, may NOT vary between client editions & versions
-// not suitable for None (transparent block)
+// only available for blocks that are non-transparent (not None)
 pub trait BlockSystem {
-    // None: no such block. Some(id): block found.
-    fn block_state_to_id(&self, state: BlockState) -> Option<Id>;
-    // None: no such state. Some(state): found state.
-    fn block_id_to_state(&self, id: Id) -> Option<BlockState>;
-
+    // Some(id): block found for this state.
+    // None: block not found. 
+    fn block_state_to_id(&self, state: BlockState) -> Option<BlockId>;
+    // Some(state): found state for this id.
+    // None: block not found. 
+    fn block_id_to_state(&self, id: BlockId) -> Option<BlockState>;
+    // None: this block does not need a buffer.
+    // Some(buf): creates a buffer for this block.
     fn buf_for_block_state(&self, state: BlockState) -> Option<BlockBuf>;
 }
 
-pub mod hash_block_system {
-    use std::collections::{HashMap, HashSet};
-    use super::{Id, BlockState, BlockBuf, BlockSystem};
-
-    #[derive(Default)]
-    pub struct HashBlockSystem<'a> {
-        state_to_id: HashMap<u16, Id<'a>>,
-        id_to_state: HashMap<Id<'a>, u16>,
-        state_should_create: HashSet<u16>,
-        next_state: u16
-    }
-
-    impl HashBlockSystem<'a> {
-        pub fn new() -> HashBlockSystem<'a> {
-            Default::default()
-        }
-
-        pub fn register_new_block_unbuffered(&mut self, id: impl Into<Id<'a>>) {
-            let id = id.into();
-            self.reg_block(id.clone());
-            self.reg_buf(id, false);
-            self.next_state += 1;
-        }
-
-        pub fn register_new_block_buffered(&mut self, id: impl Into<Id<'a>>) {
-            let id = id.into();
-            self.reg_block(id.clone());
-            self.reg_buf(id, true);
-            self.next_state += 1;
-        }
-
-        #[inline]
-        fn reg_block(&mut self, id: Id<'a>) {
-            if !self.id_to_state.contains_key(&id) {
-                self.id_to_state.insert(id.clone(), self.next_state);
-                self.state_to_id.insert(self.next_state, id);
-            }
-        } 
-
-        #[inline]
-        fn reg_buf(&mut self, id: Id, buffered: bool) {
-            let state = self.id_to_state.get(&id).unwrap();
-            match buffered {
-                true => self.state_should_create.insert(*state),
-                false => self.state_should_create.remove(state),
-            };
-        } 
-    }
-
-    impl BlockSystem for HashBlockSystem<'a> {
-
-        fn block_state_to_id(&self, state: BlockState) -> Option<Id> {
-            self.state_to_id.get(&state.runtime_internal).map(|s| s.to_owned())
-        }
-
-        fn block_id_to_state(&self, id: Id) -> Option<BlockState> {
-            self.id_to_state.get(&id).map(|&inner| BlockState { runtime_internal: inner })
-        }
-
-        fn buf_for_block_state(&self, state: BlockState) -> Option<BlockBuf> {
-            match self.state_should_create.contains(&state.runtime_internal) {
-                true => Some(vec![0; 0]),
-                false => None
-            }
-        }
-    }
-}
-
+// Allows for reading blocks from a source
 pub trait BlockRead {
+    // Ok(Some(state)): Reading successful, block state is `state`
+    // Ok(None): Reading successful, no block here
+    // Err(error): Reading failed
+    fn read_block_state(&self, pos: BlockPos) -> Result<Option<BlockState>>;
 
-    fn read_block_state(&self, pos: BlockPos) -> Result<BlockState>;
-
-    fn block_buf(&self, pos: BlockPos) -> &BlockBuf;
-
+    // Check if there is a block
     fn contains_block(&self, pos: BlockPos) -> bool;
+
+    // Callers must ensure that there is a buf.
+    // If there isn't a buf here, panic.
+    fn block_buf(&self, pos: BlockPos) -> &BlockBuf;
 }
 
+// A trait for objects thich are block-oriented sinks
 pub trait BlockWrite {
-
+    // Ok(()): Writing successful
+    // Err(error): Writing failed with error
     fn write_block_state(&mut self, pos: BlockPos, new_state: BlockState) -> Result<()>;
 
+    // Ensure that all intermediately buffered blocks reach their destination
     fn flush_blocks(&mut self) -> Result<()>;
     
+    // Must ensure that there is a buf, or program would panic
     fn block_buf_mut(&mut self, pos: BlockPos) -> &mut BlockBuf;
 }
 
+// A trait for allows opertions depending on BlockSystem
 pub trait BlockOperate {
 
-    fn get_block_system(&self) -> &dyn BlockSystem;
+    // Ensure that the block system does not change the stategy of one block
+    // during the whole lifetime of Self.
+    fn block_system(&self) -> &dyn BlockSystem;
 }
 
+// wraps a block storage, providing it with getters and setters
 pub struct BlockCursor<T>(T);
 
 impl<T> BlockCursor<T> {
@@ -148,38 +102,27 @@ impl<T: BlockWrite> BlockCursor<T> {
     }
 }
 
-impl<T: BlockOperate + BlockRead + BlockWrite> BlockCursor<T> {
-    pub fn get_block(&self, _pos: impl Into<BlockPos>) -> Result<Id<'a>> {
-        use std::borrow::Cow;
-        Ok(Id::from(Cow::from("minecraft:stone")))
-    }
-
-    pub fn set_block(&mut self, _pos: impl Into<BlockPos>, _block_id: impl Into<Id<'a>>) -> Result<()> {
-        Ok(())
+impl<T: BlockOperate + BlockRead> BlockCursor<T> {
+    // Ok(Some(id)): Reading successful, block id is `id`
+    // Ok(None): Reading successful, no block here
+    // Err(error): Reading failed
+    pub fn get_block(&self, pos: impl Into<BlockPos>) -> Result<Option<BlockId>> {
+        let pos = pos.into();
+        let state = self.0.read_block_state(pos)?; // Option<BlockId>
+        match state {
+            Some(state) => {
+                match self.0.block_system().block_state_to_id(state) {
+                    Some(id) => Ok(Some(id)),
+                    None => Err(Error::UnsupportedBlock)
+                }
+            }
+            None => Ok(None) 
+        }
     }
 }
 
-//
-
-// consider macros
-// this is a unique module
-
-mod sign {
-    use super::Result;
-    pub struct SignCursor<T>(T);
-
-    impl<T> SignCursor<T> {
-        pub fn new(t: T) -> SignCursor<T> {
-            SignCursor(t)
-        }
+impl<T: BlockOperate + BlockWrite> BlockCursor<T> {
+    pub fn set_block(&mut self, _pos: impl Into<BlockPos>, _block_id: impl Into<BlockId>) -> Result<()> {
+        Ok(())
     }
-
-    pub trait SignRead {
-        fn read_sign_lines(&self, lines_output: &mut [String; 4]) -> Result<()>;
-    }
-
-    pub trait SignWrite {
-        fn write_sign_lines(&mut self, lines_input: &[impl ToString; 4]) -> Result<()>;
-    }
-
 }
